f6f7d04379ff958db92caa543f2e62d46c92c4d4
yani <yani>
2025-12-25 18:44:10 +0800

    搭建框架

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import astro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', '.astro'],
+  },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  // JS 基础兜底
+  js.configs.recommended,
+
+  // Astro 官方推荐（保留）
+  ...astro.configs.recommended,
+
+  // Astro 文件 parser
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astro.parser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
+
+  // TypeScript：只启用「安全子集」
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+        '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
